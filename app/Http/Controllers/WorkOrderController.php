781f537019b17adb3221f111c22084607ffc8047
unknown <bagus.oetomo11@gmail.com>
2024-07-30 15:10:38 +0700

    update report wo

--- app/Http/Controllers/WorkOrderController.php
+++ app/Http/Controllers/WorkOrderController.php
@@ -944,7 +944,8 @@ class WorkOrderController extends Controller
                 if($userId !='*'){
                     $userIdString = $userId;
                 }
-                if($userId !=''){
+                // dd($userId);
+                if($userId !='*'){
                     $reportWO               = WorkOrder::with('picSupportName')
                                                 ->select('work_orders.*', 'users.name as username','finished','master_categories.name as categories_name','master_departements.name as departement_name','work_orders.level')
                                                 ->leftJoin('users','users.id','=','work_orders.user_id')
@@ -1051,6 +1052,7 @@ class WorkOrderController extends Controller
                                                 ->leftJoin('master_departements','master_departements.id','=','work_orders.departement_id')
                                                 ->leftJoin('master_kantor','master_kantor.id','=','users.kode_kantor')
                                                 ->leftJoin('master_priorities','master_priorities.id','work_orders.priority')
+                                                ->leftJoin(DB::raw('(select request_code, created_at as finished from work_order_logs where status_wo = 4) as logs'), 'logs.request_code', '=', 'work_orders.request_code')
                                                 ->where('work_orders.request_for',$requestFor->initial)
                                                 ->where('master_kantor.id','like','%'.$officeString.'%')
                                                 ->where('work_orders.status_wo','like','%'.$statusString.'%')
