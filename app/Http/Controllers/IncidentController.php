2500fe0efe7561302d7ea503e39996fafbae15bb
unknown <bagus.oetomo11@gmail.com>
2024-08-27 11:01:21 +0700

    update bug on js

--- app/Http/Controllers/IncidentController.php
+++ app/Http/Controllers/IncidentController.php
@@ -26,7 +26,7 @@ class IncidentController extends Controller
             'problemRelation',
             'categoriesRelation',
             'locationRelation',
-        ])->orderBy('created_at', 'desc')->orderBy('status','asc')->get();
+        ])->orderBy('status','asc')->get();
         return response()->json([
             'data'=>$data,
         ]);
